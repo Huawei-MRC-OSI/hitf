ba374614ec167aa7cd4a81866d8452ddee23c9d6
Sergey Mironov <mironov.sergey@huawei.com>
2018-12-24 12:50:25 +0300

    Add port mapping options

--- rundocker.sh
+++ rundocker.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+while test -n "$1" ; do
+  case "$1" in
+    -h|--help)
+      echo "Usage: $0 [--map-sockets]" >&2
+      exit 1
+      ;;
+    --map-sockets)
+      MAPSOCKETS=y
+      ;;
+    *)
+      SUFFIX="$1"
+      ;;
+  esac
+  shift
+done
+
 CWD=$(cd `dirname $0`; pwd;)
 UID=`id --user`
 
@@ -19,6 +35,18 @@ docker build \
   -t hitf \
   -f $CWD/docker/Dockerfile.dev $CWD/docker
 
+
+if test "$MAPSOCKETS" = "y"; then
+  PORT_TENSORBOARD=`expr 6000 + $UID - 1000`
+  PORT_JUPYTER=`expr 8000 + $UID - 1000`
+  DOCKER_PORT_ARGS="-p 0.0.0.0:$PORT_TENSORBOARD:6006 -p 0.0.0.0:$PORT_JUPYTER:8888"
+  echo
+  echo "*****************************"
+  echo "Your Jupyter port: ${PORT_JUPYTER}"
+  echo "Your Tensorboard port: ${PORT_TENSORBOARD}"
+  echo "*****************************"
+fi
+
 docker --config "$DOCKER_CFG" \
     run -it \
     --volume $CWD:/workspace \
@@ -30,6 +58,7 @@ docker --config "$DOCKER_CFG" \
     -e "CI_BUILD_GROUP=$(id -g -n)" \
     -e "CI_BUILD_GID=$(id -g)" \
     -e "DISPLAY=$DISPLAY" \
+    ${DOCKER_PORT_ARGS} \
     hitf \
     bash --login /install/with_the_same_user.sh bash
 
